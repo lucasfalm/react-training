f55ce556c3448e7857a999c28a490f8939fb1ac0
flucas97 <feijolucas1997@gmail.com>
2020-07-05 20:19:11 -0300

    create card layout, and wrap all children elements to the component

--- react-trainning/src/App.jsx
+++ react-trainning/src/App.jsx
@@ -3,13 +3,25 @@ import Header from './components/basics/Header'
 import Subheader from  './components/basics/Subheader'
 import Welcome from './components/basics/Welcome'
 import Random from './components/basics/Random'
+import Card from './components/layout/Card'
 
 // simpliest way to write an arrow function
 export default _ => // _ with 1 ignored param, could be (), or (props)  
         <div id="App">
-            <Header />
-            <Subheader name="Lucas" />
-            <Welcome />
-            <Random max = { 20 }  min = { 50 }></Random>
+            <Card title="Welcome Name"> 
+                <Header />
+            </Card>
+
+            <Card title="Welcome Name"> 
+                <Subheader name="Lucas" />
+            </Card>
+
+            <Card title="Welcome"> 
+                <Welcome />
+            </Card>
+
+            <Card title="Random"> 
+                <Random max = { 20 }  min = { 50 } />
+            </Card>
         </div>
 // no returns because the arrow implicitly returns the last evaluetade value
