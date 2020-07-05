3c48f53c648741dcc129f3ca273efbd1e57f718b
flucas97 <feijolucas1997@gmail.com>
2020-07-05 16:57:21 -0300

    rename params for props

--- react-trainning/src/components/basics/Subheader.jsx
+++ react-trainning/src/components/basics/Subheader.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
-export default function Subheader(params) {
+export default function Subheader(props) {
     return(
-    <div><h2> Hello, { params.name }</h2></div>
+        <div>
+            <h2> Hello, { props.name }</h2>
+        </div>
     )
 }
