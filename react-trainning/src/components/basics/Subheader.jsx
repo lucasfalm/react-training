db2fd720f0bcfb3a328b3e48a179c51b01b2c57c
flucas97 <feijolucas1997@gmail.com>
2020-07-05 16:55:55 -0300

    pass parameters to components

---
+++ react-trainning/src/components/basics/Subheader.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Subheader(params) {
+    return(
+    <div><h2> Hello, { params.name }</h2></div>
+    )
+}
--- react-trainning/src/index.js
+++ react-trainning/src/index.js
@@ -2,13 +2,14 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 import './index.css'
 import Header from './components/basics/Header'
+import Subheader from  './components/basics/Subheader'
 
-const tag =  <h1> Hello world from React! </h1>
+const tag =  <h3> Welcome to React App! </h3>
 
 ReactDOM.render(
-    
     <div> 
         <Header></Header>
+        <Subheader name="Lucas"></Subheader>
         { tag }
     </div>,
     document.getElementById('root')
