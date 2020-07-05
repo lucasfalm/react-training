8d1abc18f749928ecc5aeaed94d44442e2d19707
flucas97 <feijolucas1997@gmail.com>
2020-07-05 15:10:27 -0300

    use tag and JSX

--- react-trainning/src/index.js
+++ react-trainning/src/index.js
@@ -1,7 +1,8 @@
 import ReactDOM from 'react-dom'
+import React from 'react'
 
-
+const tag =  <h1> Hello world from React! </h1>
 ReactDOM.render(
-    "Hello world from React!",
+    <div> { tag } </div>,
     document.getElementById('root')
 )
