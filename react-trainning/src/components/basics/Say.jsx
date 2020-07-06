e8ad1c4e53cc5fb4fc738c7c9a0c796f561b6ccb
flucas97 <feijolucas1997@gmail.com>
2020-07-06 13:16:55 -0300

    change var names

--- react-trainning/src/components/basics/Say.jsx
+++ react-trainning/src/components/basics/Say.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import If, { Else } from './If'
 
 export default props => { 
-    const what = props.what || ""
+    const say = props.something || {}
 
     return (
         <div>
-            <If content={ what }>
+            <If content={ say.what }>
                 <h3> The lord say:</h3>
-                <span><strong> { what } </strong></span> 
+                <span><strong> { say.what } </strong></span> 
                 <Else>
                     <span> I don't like you. Nothing to say.</span>
                 </Else>
