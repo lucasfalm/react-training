9bed140fde625bca2b15ca5104c895f3dec984e4
flucas97 <feijolucas1997@gmail.com>
2020-07-06 02:43:01 -0300

    card opacity

--- react-trainning/src/components/basics/Random.jsx
+++ react-trainning/src/components/basics/Random.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 
-export default (props) => {
+export default props => {
     const { max, min } = props // js knows that props is an object, and will search .max and .min inside of it
-    const rounded = Math.round(Math.random() * (max - min) + min)
-    const isOdd = rounded % 2 === 0 
+    const rounded = ( Math.round(Math.random() * (max - min) + min) )
+    const isOdd = ( rounded % 2 === 0 )
+
     return (
      <div>
-         <strong>{ rounded } </strong> 
+         <strong> { rounded } </strong> 
          { isOdd ?
             <span> Odd </span> :
             <span> Even </span>
