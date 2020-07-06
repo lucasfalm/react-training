da301e9bace3e45e2448da930c8da3de70b1b775
flucas97 <feijolucas1997@gmail.com>
2020-07-05 22:20:12 -0300

    apply flex content as sytle for components

--- react-trainning/src/App.jsx
+++ react-trainning/src/App.jsx
@@ -4,24 +4,27 @@ import Subheader from  './components/basics/Subheader'
 import Welcome from './components/basics/Welcome'
 import Random from './components/basics/Random'
 import Card from './components/layout/Card'
+import './App.css'
 
 // simpliest way to write an arrow function
 export default _ => // _ with 1 ignored param, could be (), or (props)  
-        <div id="App">
-            <Card title="Header"> 
-                <Header />
-            </Card>
+        <div className="App">
+            <div className="Cards">
+                <Card title="Header"> 
+                    <Header />
+                </Card>
 
-            <Card title="Welcome Name"> 
-                <Subheader name="Lucas" />
-            </Card>
+                <Card title="Welcome Name"> 
+                    <Subheader name="Lucas" />
+                </Card>
 
-            <Card title="Welcome without props"> 
-                <Welcome />
-            </Card>
+                <Card title="Welcome without props"> 
+                    <Welcome />
+                </Card>
 
-            <Card title="Random"> 
-                <Random max = { 20 }  min = { 50 } />
-            </Card>
+                <Card title="Random"> 
+                    <Random max = { 20 }  min = { 50 } />
+                </Card>
+            </div>
         </div>
 // no returns because the arrow implicitly returns the last evaluetade value
