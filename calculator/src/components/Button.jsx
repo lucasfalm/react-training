dff48981d1accf36869e13e6dd7b435f7a514179
flucas97 <feijolucas1997@gmail.com>
2020-07-08 14:37:08 -0300

    start implementing logic

--- calculator/src/components/Button.jsx
+++ calculator/src/components/Button.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import './Button.css'
 
 export default props => { 
-    return ( 
+    let classes = 'button '
+    classes += props.operation ? 'operation' : ''
+    classes += props.double ? 'double' : ''
+    classes += props.triple ? 'triple' : ''
+    return (
         <>
-            <button>{ props.label }</button>
+            <button 
+                className={classes}
+                onClick={ e => props.click(e.target.innerHTML)}
+            >
+                { props.label }
+            </button>
         </>
     )
 }
--- calculator/src/main/Calculator.jsx
+++ calculator/src/main/Calculator.jsx
@@ -5,27 +5,39 @@ import Display from '../components/Display'
 import './Calculator.css'
 
 class Calculator extends Component {
+    state = {
+        total: 0,
+        equation: ''
+    }
+
+    calculate = (value) => {
+        let actualEquation = this.state.equation
+        actualEquation += value
+        this.setState({
+            equation: actualEquation,
+        })
+    }
 
     render() { 
         return(
             <div className="calculator">
-               <Display value={ 2 } />
-               <Button label="AC" />
-               <Button label="/" />
-               <Button label="7" />
-               <Button label="8" />
-               <Button label="9" />
-               <Button label="*" />
-               <Button label="4" />
-               <Button label="5" />
-               <Button label="6" />
-               <Button label="-" />
-               <Button label="2" />
-               <Button label="3" />
-               <Button label="+" />
-               <Button label="0" />
-               <Button label="." />
-               <Button label="=" />
+               <Display value={ this.state.equation } />
+               <Button label="/" click={ this.calculate } />
+               <Button label="AC" click={ this.calculate }  />
+               <Button label="7" click={ this.calculate } />
+               <Button label="8" click={ this.calculate } />
+               <Button label="9" click={ this.calculate } />
+               <Button label="*" click={ this.calculate } />
+               <Button label="4" click={ this.calculate } />
+               <Button label="5" click={ this.calculate } />
+               <Button label="6" click={ this.calculate } />
+               <Button label="-" click={ this.calculate } />
+               <Button label="2" click={ this.calculate } />
+               <Button label="3" click={ this.calculate } />
+               <Button label="+" click={ this.calculate } />
+               <Button label="0" click={ this.calculate } />
+               <Button label="." click={ this.calculate } />
+               <Button label="=" click={ this.calculate } />
             </div>
         )
     }     
