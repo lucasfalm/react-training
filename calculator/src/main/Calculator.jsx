e44ae34dffb52b69305114db024e01aa0db2471a
flucas97 <feijolucas1997@gmail.com>
2020-07-08 13:00:19 -0300

    create Display and Button component, with style

--- calculator/src/main/Calculator.jsx
+++ calculator/src/main/Calculator.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import Button from '../components/Button'
+import Display from '../components/Display'
+
 import './Calculator.css'
 
 class Calculator extends Component {
@@ -6,7 +9,23 @@ class Calculator extends Component {
     render() { 
         return(
             <div className="calculator">
-
+               <Display value={ 2 } />
+               <Button label="AC" />
+               <Button label="/" />
+               <Button label="7" />
+               <Button label="8" />
+               <Button label="9" />
+               <Button label="*" />
+               <Button label="4" />
+               <Button label="5" />
+               <Button label="6" />
+               <Button label="-" />
+               <Button label="2" />
+               <Button label="3" />
+               <Button label="+" />
+               <Button label="0" />
+               <Button label="." />
+               <Button label="=" />
             </div>
         )
     }     
