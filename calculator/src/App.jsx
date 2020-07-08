e228849a9f0346dc1593e9ec2ba18edf781a9417
flucas97 <feijolucas1997@gmail.com>
2020-07-08 12:41:01 -0300

    project structure

---
+++ calculator/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import Calculator from './main/Calculator'
+
+export default _ => {
+  return (
+    <div className="App">
+      <Calculator />
+    </div>
+  );
+}
