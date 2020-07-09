3f41c8f8161a026dc5592fd67f9427932019d2bd
flucas97 <feijolucas1997@gmail.com>
2020-07-08 22:22:00 -0300

    implement calculator logic, still necessary refactor for clean code

--- calculator/src/components/Display.jsx
+++ calculator/src/components/Display.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import './Display.css'
 
-export default props => { 
+export default props => {
+    let display = ''
+    if( props.total != '') { 
+        display = props.total
+    } else { 
+        display =  props.leftHand + props.equation + props.rightHand
+    }
     return( 
         <div className="display">
-            { props.value }
+            { display }
         </div>
     )
 }
--- calculator/src/main/Calculator.jsx
+++ calculator/src/main/Calculator.jsx
@@ -6,41 +6,130 @@ import './Calculator.css'
 
 class Calculator extends Component {
     state = {
-        total: 0,
-        equation: ''
+        total: '',
+        equation: '',
+        flag: false,
+        leftHand: '',
+        rightHand: ''
     }
 
-    calculate = (newValue) => {
-        let actual = this.state.equation
-        let update = actual += newValue
+    sub = () => {        
+        if(this.state.flag) {
+            let total =+ makeCalc(this.state.leftHand, this.state.rightHand, this.state.equation)
+            this.prepareOperation(false, total, '-')
+        } else {
+            this.prepareOperation(true, this.state.total, '-')
+        }
+    }
 
+    add = () => {        
+        if(this.state.flag) {
+            let total =+ makeCalc(this.state.leftHand, this.state.rightHand, this.state.equation)
+            this.prepareOperation(false, total, '+')
+        } else {
+            this.prepareOperation(true, this.state.total, '+')
+        }
+    }
+
+    mult = () => {        
+        if(this.state.flag) {
+            let total =+ makeCalc(this.state.leftHand, this.state.rightHand, this.state.equation)
+            this.prepareOperation(false, total, '*')
+        } else {
+            this.prepareOperation(true, this.state.total, '*')
+        }
+    }
+
+    div = () => {        
+        if(this.state.flag) {
+            let total =+ makeCalc(this.state.leftHand, this.state.rightHand, this.state.equation)
+            this.prepareOperation(false, total, '/')
+        } else {
+            this.prepareOperation(true, this.state.total, '/')
+        }
+    }
+
+    prepareOperation = (status, total, equationType) => { 
         this.setState({
-            equation: update,
+            total: total,
+            flag: status,
+            equation: equationType,
         })
     }
+
+
+    calculate = (value) => {     
+        if(this.state.flag) { 
+            let actual = this.state.rightHand
+            this.setState({ rightHand: actual + value })
+        } else {
+            let actual = this.state.leftHand
+            this.setState({ leftHand: actual + value })
+        }
+    }
+
+    total = () => { 
+        let total = makeCalc(this.state.leftHand, this.state.rightHand, this.state.equation)
+        console.log(total)
+        return (
+            this.setState({ 
+                total:  total,
+                leftHand: total,
+                rightHand: '',
+                equation: ''
+            })
+        )
+    }
+
+    clear = () => { this.setState({ total: 0, equation: '', flag: false, leftHand: '', rightHand: ''})}
 
     render() { 
         return(
             <div className="calculator">
-               <Display value={ this.state.equation } />
-               <Button label="/" click={ this.calculate } />
-               <Button label="AC" click={ this.calculate }  />
-               <Button label="7" click={ this.calculate } />
-               <Button label="8" click={ this.calculate } />
-               <Button label="9" click={ this.calculate } />
-               <Button label="*" click={ this.calculate } />
+               <Display total= { this.state.total } leftHand={ this.state.leftHand } equation={ this.state.equation }  rightHand={ this.state.rightHand }/>
+               <Button label="1" click={ this.calculate } />
+               <Button label="2" click={ this.calculate } />
+               <Button label="3" click={ this.calculate } />
+               <Button label="AC" click={ this.clear }  />
                <Button label="4" click={ this.calculate } />
                <Button label="5" click={ this.calculate } />
                <Button label="6" click={ this.calculate } />
-               <Button label="-" click={ this.calculate } />
-               <Button label="2" click={ this.calculate } />
-               <Button label="3" click={ this.calculate } />
-               <Button label="+" click={ this.calculate } />
+               <Button label="/" click={ this.div } />
+               <Button label="7" click={ this.calculate } />
+               <Button label="8" click={ this.calculate } />
+               <Button label="9" click={ this.calculate } />
+               <Button label="*" click={ this.mult } />
                <Button label="0" click={ this.calculate } />
+               <Button label="-" click={ this.sub } />
+               <Button label="+" click={ this.add } />
+               <Button label="=" click={ this.total } />
                <Button label="." click={ this.calculate } />
-               <Button label="=" click={ this.calculate } />
             </div>
         )
     }     
 }
 export default Calculator
+
+function makeCalc(left, right, expression) { 
+    let result = 0
+    let avaliableExpressions = ['-', '+', '*', '/']
+    for(var i=0; i<= avaliableExpressions.length; i++) { 
+        if(expression === avaliableExpressions[i]) { 
+            switch(i) {
+                case 0:
+                    result = parseInt(left) - parseInt(right)
+                    break
+                case 1:
+                    result =  parseInt(left) + parseInt(right)
+                    break
+                case 2:
+                    result =  parseInt(left) * parseInt(right)
+                    break  
+                case 3:
+                    result =  parseInt(left) / parseInt(right)
+                    break
+            }
+        }
+    }
+    return result
+}
