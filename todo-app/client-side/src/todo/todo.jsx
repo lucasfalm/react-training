94c95eadf0b192bf42e86e779694f30aeee0eb85
flucas97 <feijolucas1997@gmail.com>
2020-07-28 20:55:26 -0300

    start configuring Todo class

---
+++ todo-app/client-side/src/todo/todo.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Todo extends Component {
+
+    render() { 
+
+        return (
+            <div>
+                <h1> Teste </h1>
+            </div>
+        )
+    }
+}
