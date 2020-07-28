a9b8cbc8a7a64e4ff159f05b1576383f20103a4a
flucas97 <feijolucas1997@gmail.com>
2020-07-28 20:08:21 -0300

    configure app jsx

---
+++ todo-app/client-side/src/main/app.jsx
@@ -0,0 +1,12 @@
+import 'modules/bootstrap/dist/css/bootstrap.min.css'
+import 'modules/font-awesome/css/font-awesome.min.css'
+import '../template/custom.css'
+
+import React from 'react'
+
+
+export default props => (
+    <div className='container'>
+
+    </div>
+)
