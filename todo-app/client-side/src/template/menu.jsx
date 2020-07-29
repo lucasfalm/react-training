2d8129bd027898d95349c46414e6e332631457f2
flucas97 <feijolucas1997@gmail.com>
2020-07-28 21:48:02 -0300

    fix menu, with bg black

--- todo-app/client-side/src/template/menu.jsx
+++ todo-app/client-side/src/template/menu.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 
 export default _ => 
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
         <a class="navbar-brand" href="#"> <strong>TODO App</strong></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <ul class="navbar-nav">
                 <li class="nav-item active">
-                    <a class="nav-link" href="#">About <span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="#">Tarefas <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Sobre <span class="sr-only">(current)</span></a>
                 </li>
             </ul>
         </div>
