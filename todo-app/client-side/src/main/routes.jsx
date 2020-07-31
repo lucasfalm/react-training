ecaf96e193a63f64c8cdb1f6a82f3d0bff03b97d
flucas97 <feijolucas1997@gmail.com>
2020-07-31 18:01:04 -0300

    configure all routes and fix no content bug

--- todo-app/client-side/src/main/routes.jsx
+++ todo-app/client-side/src/main/routes.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import { Router, Route, Redirect } from 'react-router'
-import history from './history';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Redirect } from 'react-router'
 import Todo from '../todo/todo'
 import About from '../about/about'
-import App from './App'
 
 export default _ => (
-    <Router history={ history }>
-        <Route path='/' component={ App }>
-            <Route path='todos' component={ Todo } />
-            <Route path='about' component={ About } />
-            <Redirect from='*' to='/' />
-        </Route>
-    </Router>
+    <BrowserRouter>
+        <Switch>
+            <Route path='/' exact component={ Todo } />
+            <Route path='/todos' exact component={ Todo } />
+            <Route path='/about' exact component={ About } />
+            <Redirect path='*' to='/' />
+        </Switch>
+    </BrowserRouter>
 )
--- todo-app/client-side/src/template/menu.jsx
+++ todo-app/client-side/src/template/menu.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 
 export default _ => 
     <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
-        <a class="navbar-brand" href="#"> <strong>TODO App</strong></a>
+        <a class="navbar-brand" href="/"> <strong>TODO App</strong></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <ul class="navbar-nav">
                 <li class="nav-item active">
-                    <a class="nav-link" href="#/todos">Tarefas</a>
+                    <a class="nav-link" href="/todos">Tarefas</a>
                 </li>
                 <li class="nav-item active">
-                    <a class="nav-link" href="#/about">Sobre</a>
+                    <a class="nav-link" href="/about">Sobre</a>
                 </li>
             </ul>
         </div>
